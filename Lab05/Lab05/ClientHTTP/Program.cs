b28542767bdf6a7376a4b59c4348927dce7271f5
alafyte <xleb.xlebnyj@mail.ru>
2024-11-09 14:53:16 +0500

    Лабораторная работа №5

---
+++ Lab05/Lab05/ClientHTTP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClientHTTP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lab05.A a = new Lab05.A(), b = new Lab05.A();
+            a.f = 3.1f;    b.f = 28.6f;
+            a.k = 23;    b.k = -2;
+            a.s = "ab";  b.s = "ba";
+      
+            WCFSimplexClient client = new WCFSimplexClient();
+
+            Console.WriteLine(client.Add(78, 23));
+            Console.WriteLine(client.Concat("Hello, ", "World!"));
+            
+            Lab05.A result = client.Sum(a, b);
+            Console.WriteLine($"f: {result.f}\nk: {result.k}\ns: {result.s}");
+
+            client.Close();
+        }
+    }
+}
---
+++ Lab05/Lab05/Lab05/WCFSimplex.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab05
+{
+    public class WCFSimplex : IWCFSimplex
+    {
+        public int Add(int a, int b) => a + b;
+        public string Concat(string a, string b) => a + b;
+        public A Sum(A a, A b) => a + b;
+    }
+}
