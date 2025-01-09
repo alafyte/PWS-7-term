053c63f9774c0d4ccd950355e5ff81d66aba9a16
alafyte <xleb.xlebnyj@mail.ru>
2025-01-09 17:55:51 +0300

    Лабораторная работа №3

---
+++ Lab03/Lab03/Lab03/App_Data/create-database.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Students] (
+    [Id]    INT           IDENTITY (1, 1) NOT NULL,
+    [Name]  NVARCHAR (30) NOT NULL,
+    [Phone] NVARCHAR (30) NOT NULL,
+    CONSTRAINT [PK_Students] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
